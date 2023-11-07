839931cd780e06633a05f360fca9093b4e27eebc
Andriw Fonseca <andriw.fonseca@totvs.com.br>
2023-11-07 16:41:05 -0300

    feat: criado o insert de dados na tabela

--- src/relatorio-fies/services/relatorio-fies.services.ts
+++ src/relatorio-fies/services/relatorio-fies.services.ts
@@ -13,11 +13,50 @@ export class RelatorioFiesService {
     public async importCsvAndPopulateTable () {   
         console.log('Iniciando carregamento do csv');
         try {
+            let count = 0;
             for (const item of dataCsv) { 
-                console.log(item, 'item')
+                if(count > 0) return;
+                const dataNascimentoParts = item.DatadeNascimento.split('/');
+                const dataNascimento = new Date(
+                    parseInt(dataNascimentoParts[2]),
+                    parseInt(dataNascimentoParts[1]) - 1,
+                    parseInt(dataNascimentoParts[0])
+                );
+            
+                await this.prisma.inscricao_fies.create({
+                    data: {
+                        ano_processo_seletivo: item.Anodoprocessoseletivo,
+                        semestre_processo_seletivo: item.Semestredoprocessoseletivo,
+                        sexo: item.Sexo,
+                        data_nascimento: dataNascimento,
+                        uf_residencia: item['UFderesidência'],
+                        municipio_residencia: item['Municipioderesidência'],
+                        etnia_cor: item['Etnia/Cor'],
+                        pessoa_deficiente: item['Pessoacomdeficiência?'],
+                        ano_conclusao_ensino_medio: item['Anoconclusãoensinomédio'],
+                        concluiu_curso_superior: item['Concluiucursosuperior?'],
+                        numero_membros_grupo_familiar: parseInt(item['NºdemembrosGrupoFamiliar']),
+                        renda_familiar_mensal_bruta: parseFloat(item.Rendafamiliarmensalbruta.replace(',', '.')),
+                        regiao_grupo_preferencia: item['Regiãogrupodepreferência'],
+                        uf: item.UF,
+                        conceito_curso_gp: parseInt(item['ConceitodecursodoGP']),
+                        area_conhecimento: item['Áreadoconhecimento'],
+                        sub_area_conhecimento: item['Subáreadoconhecimento'],
+                        nota_corte_grupo_preferencia: parseFloat(item['NotaCorteGrupoPreferência'].replace(',', '.')),
+                        media_nota_enem: parseFloat(item['MédianotaEnem'].replace(',', '.')),
+                        ano_enem: parseInt(item['AnodoEnem']),
+                        redacao: parseInt(item['Redação']),
+                        situacao_inscricao_fies: item['SituaçãoInscriçãoFies'],
+                        percentual_financiamento: item.Percentualdefinanciamento !== '' ? parseFloat(item.Percentualdefinanciamento) : null,
+                        semestre_financiamento: item.Semestredofinanciamento !== ' ' ? parseInt(item.Semestredofinanciamento) : null,
+                        qtde_semestre_financiado: item.Qtdesemestrefinanciado !== ' ' ? parseInt(item.Qtdesemestrefinanciado) : null,
+                    }
+                });
+                count++;
+                console.log(item, 'item');
             }
         } catch (error) {
-            console.log(error, 'erro')
+            console.error(error, 'erro');
         }
         
         console.log('Processo finalizado com sucesso!!');        
